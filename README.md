cb9c970b69ff67c6cda7f60fbc9256e3aa03126a
gompro <leejh3224@naver.com>
2022-10-30 01:21:54 +0900

    feat: works both on full screen & windowed mode

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Useful hotkeys for Victoria Revolutions in Python
 ## List of hotkeys
 
 - `z`: split pops
-- `x`: add clear
+- `x`: add clerks
 - `c`: add craftsman
 - `space`: pause game
+- `q`: exit program
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import pyautogui
 import keyboard
-import time
+
+def move_to(x, y):
+    window = get_window()
+    (top_x, top_y) = window.topleft
+
+    is_windowed_mode = window.size.width != 1024 and window.size.height != 768
+    screen_title_offset = 31 if is_windowed_mode else 0
+
+    pyautogui.moveTo(top_x + x, top_y + y + screen_title_offset)
+
+def add_pops(n):
+    # add pops into rgo
+    # trick: pops will be added according to their size (bigger pops will be added first)
+    pyautogui.press('a', presses=n)
 
 def get_window():
     windows = pyautogui.getWindowsWithTitle("Victoria")
@@ -10,32 +23,28 @@ def get_window():
     raise AssertionError("Failed to find window with title - Victoria")
 
 def select_rgo():
-    window = get_window()
-    (top_x, top_y) = window.topleft
-
-    pyautogui.moveTo(top_x + 200, top_y + 325)
+    rgo_box_pos = (35, 146)
+    move_to(*rgo_box_pos)
     pyautogui.click()
 
 def close_and_open():
-    window = get_window()
-    (top_x, top_y) = window.topleft
+    open_rgo_btn_pos = (155, 174)
 
-    pyautogui.moveTo(top_x + 300, top_y + 400)
+    # closing and then reopening rgo will empty it
+    move_to(*open_rgo_btn_pos)
     pyautogui.click()
     pyautogui.click()
-
-def go_to_first_pop():
-    window = get_window()
-    (top_x, top_y) = window.topleft
-    (_, mid_y) = window.midleft
-
-    pyautogui.moveTo(top_x + 125, mid_y + 70)
 
 def go_back():
-    window = get_window()
-    (top_x, top_y) = window.topleft
+    go_to_rgo_btn_pos = (125, 110)
+    move_to(*go_to_rgo_btn_pos)
+    pyautogui.click()
 
-    pyautogui.moveTo(top_x + 200, top_y + 270)
+def get_first_pop_pos():
+    return (125, 399)
+
+def select_first_pop():
+    move_to(*get_first_pop_pos())
     pyautogui.click()
 
 def pause(key):
@@ -49,85 +58,63 @@ def split_pops(key):
     split pops for both RGOs and Factories
     You should select a RGO or Factory first in order for this script to work
     """
-    window = get_window()
-    (top_x, top_y) = window.topleft
-    (_, mid_y) = window.midleft
+    max_visible_pops = 5
+    split_pop_btn_pos = (52, 245)
 
     select_rgo()
     close_and_open()
 
-    # add unsplited pops
-    pyautogui.press('a', presses=3)
+    add_pops(n=max_visible_pops)
+    pyautogui.sleep(0.08)
 
-    go_to_first_pop()
-
-    for i in range(3):
-
-        # go next pop
-        pyautogui.moveTo(top_x + 125, mid_y + 70 + 60 * i)
-        time.sleep(0.1)
+    for i in range(max_visible_pops):
+        pop_offset = 40
+        first_pop_pos = get_first_pop_pos()
+    
+        move_to(first_pop_pos[0], first_pop_pos[1] + (i * pop_offset))
         pyautogui.click()
 
-        # press split
-        pyautogui.moveTo(top_x + 100, mid_y - 250)
-        for i in range(4):
+        move_to(*split_pop_btn_pos)
+        for _ in range(5):
             pyautogui.click()
 
         go_back()
-    
-    # go back to rgo
-    pyautogui.moveTo(top_x + 200, top_y + 245)
-    pyautogui.click()
 
+def _add_new(pop_class):
+    select_rgo()
+    close_and_open()
+    add_pops(n=1)
+    select_first_pop()
+    if pop_class == "craftsman":
+        move_to(153, 210)
+        pyautogui.click()
+    elif pop_class == "clerk":
+        move_to(153, 190)
+        pyautogui.click()
+    go_back()
+        
 def add_craftsman(key):
     """
     Change a pop into craftsman
     You should select a RGO or Factory first in order for this script to work
     """
-    window = get_window()
-    (top_x, top_y) = window.topleft
-    (_, mid_y) = window.midleft
-
-    select_rgo()
-    close_and_open()
-
-    # add unsplited pops
-    pyautogui.press('a', presses=1)
-
-    go_to_first_pop()
-
-    # convert to craftsman
-    pyautogui.moveTo(top_x + 350, mid_y - 325)
-    pyautogui.click()
-
-    go_back()
+    _add_new(pop_class="craftsman")
 
 def add_clerk(key):
     """
     Change a pop into cleark
     You should select a RGO or Factory first in order for this script to work
     """
-    window = get_window()
-    (top_x, top_y) = window.topleft
-    (_, mid_y) = window.midleft
-
-    select_rgo()
-    close_and_open()
-
-    # add unsplited pops
-    pyautogui.press('a', presses=1)
-
-    go_to_first_pop()
-
-    # convert to clerk
-    pyautogui.moveTo(top_x + 350, mid_y - 375)
-    pyautogui.click()
-
-    go_back()
+    _add_new(pop_class="clerk")
 
 def main():
-
-    print('script is running')
+    print('pyvickeys is running\n')
+    print('** help **')
+    print('space - pause')
+    print('z - split pops')
+    print('x - add clerks')
+    print('c - add craftsman')
+    print('q - exit program')
 
     # move faster
     pyautogui.PAUSE = 0.03
@@ -138,7 +125,8 @@ def main():
     keyboard.on_press_key("x", add_clerk)
     keyboard.on_press_key("c", add_craftsman)
 
-    keyboard.wait()
+    # exits when hotkey is pressed
+    keyboard.wait(hotkey="q")
 
 if __name__ == "__main__":
     main()
